ad106128232b9f4d3d7e1f5f33279227bc6ad653
arekstasko <arek.stasko@gmail.com>
2024-02-12 07:57:27 +0100

    Add Note Service methods

--- MyDiaryAPI.Services/Services/NoteService/INoteService.cs
+++ MyDiaryAPI.Services/Services/NoteService/INoteService.cs
@@ -7,7 +7,7 @@ public interface INoteService
 {
     public Task<bool> Create(CreateNote note);
     public Task<bool> Update(UpdateNote note);
-    public Task<List<INote>> Get();
+    public List<INote> Get();
     public Task<INote> GetById(int id);
-    public Task<INote> GetRandom();
+    public INote GetRandom();
 }
--- MyDiaryAPI.Services/Services/NoteService/NoteService.cs
+++ MyDiaryAPI.Services/Services/NoteService/NoteService.cs
@@ -1,4 +1,6 @@
-﻿using MyDiaryAPI.Domain.Models;
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using MyDiaryAPI.Domain.Models;
 using MyDiaryAPI.Persistance.Repositories;
 using MyDiaryAPI.Services.Requests;
 
@@ -6,49 +8,84 @@ namespace MyDiaryAPI.Services.Services.NoteService;
 
 public class NoteService : INoteService
 {
-    private INotesRepository _repository;
+    private readonly INotesRepository _repository;
+    private readonly IMapper _mapper;
+    private readonly ILogger<INoteService> _logger;
+    static Random rnd = new Random();
     public NoteService(INotesRepository repository)
     {
         repository = _repository;
     }
-    public Task<bool> Create(CreateNote Note)
+    public async Task<bool> Create(CreateNote Note)
     {
         try
         {
-            throw new NotImplementedException();
+            var noteToUpdate = _mapper.Map<CreateNote, INote>(Note);
+            var result = await _repository.Create(noteToUpdate);
+            return result;
         }
         catch (Exception e)
         {
-            Console.WriteLine(e);
-            throw;
+            _logger.LogError(e, "Create method in Notes Service has failed while processing");
+            return false;
         }
     }
 
-    public Task<bool> Update(UpdateNote Note)
+    public async Task<bool> Update(UpdateNote Note)
     {
         try
         {
-            //var result = _repository.
+            var noteToUpdate = _mapper.Map<UpdateNote, INote>(Note);
+            var result = await _repository.Update(noteToUpdate);
+            return result;
         }
         catch (Exception e)
         {
-            Console.WriteLine(e);
-            throw;
+            _logger.LogError(e, "Update method in Notes Service has failed while processing");
+            return false;
         }
     }
 
-    public Task<List<INote>> Get()
+    public List<INote> Get()
     {
-        throw new NotImplementedException();
+        try
+        {
+            var notes = _repository.Get();
+            return notes;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Get method in Notes Service has failed while processing");
+            throw;
+        }
     }
 
     public Task<INote> GetById(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var note = _repository.Get(id);
+            return note;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Get by Id method in Notes Service has failed while processing");
+            throw;
+        }
     }
 
-    public Task<INote> GetRandom()
+    public INote GetRandom()
     {
-        throw new NotImplementedException();
+        try
+        {
+            var notes = _repository.Get();
+            int r = rnd.Next(notes.Count);
+            return notes[r];
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Get random Note method in Notes Service has failed while processing");
+            throw;
+        }
     }
 }
